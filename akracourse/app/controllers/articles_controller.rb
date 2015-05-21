2d3ea39fb28fa79315c9a788a2cad1abff479407
adam94pk <adamzadora94@gmail.com>
2015-05-21 18:06:33 +0000

    koniec zajec

--- akracourse/app/controllers/articles_controller.rb
+++ akracourse/app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
+    def index
+    @article = Article.all
+    end
+
+    
+    def show
+       @article = Article.find(params[:id])
+        
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:article].inspect
-
+        @article = Article.new(articles_params)
+        @article.save
+        
+        redirect_to @article
     end
     
+    private
+        def articles_params
+            params.require(:article).permit(:title, :text)
+        end
+        
 end
