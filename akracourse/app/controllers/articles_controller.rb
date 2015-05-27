0db5c55a274971baccb0e7d406ba9b59177a3f87
adam94pk <adamzadora94@gmail.com>
2015-05-27 12:12:38 +0000

    Add view partial in new

--- akracourse/app/controllers/articles_controller.rb
+++ akracourse/app/controllers/articles_controller.rb
@@ -2,13 +2,17 @@ class ArticlesController < ApplicationController
     def index
     @article = Article.all
     end
-
     
     def show
        @article = Article.find(params[:id])
     end
-    
+
     def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
@@ -20,9 +24,21 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(articles_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
+    
     private
         def articles_params
             params.require(:article).permit(:title, :text)
         end
+        
         
 end
